217c058f37b62406053e576462a5092573bc40af
xipingya <xiping.yan@intel.com>
2024-07-01 15:14:42 +0800

    add printf

--- cpp_abort_with_backtrace/main.cpp
+++ cpp_abort_with_backtrace/main.cpp
@@ -1,12 +1,20 @@
-#include <iostream>
+// #include <iostream>
 #include <memory>
 
 // Call boost to print backtrace
 #define BOOST_STACKTRACE_USE_ADDR2LINE
 #include <boost/stacktrace.hpp>
+#include <iostream>
 void print_stacktrace(void)
 {
-	std::cout << boost::stacktrace::stacktrace();
+	std::cout << boost::stacktrace::stacktrace() << std::endl;
+
+	// without: <iostream>
+	auto vec = boost::stacktrace::stacktrace().as_vector();
+	for (size_t i = 0; i < vec.size(); i++)
+	{
+		printf(" %ld# %s\n", i, vec[i].name().c_str());
+	}
 }
 
 void fun3()
